3b5099ebb17542af21972699b3d14ad2d48e592c
moamen270 <mmoamen270@gmail.com>
2021-09-08 16:58:54 +0200

    New Tool

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASCII_Art
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("\n This is ASCII Art Program \n =============================\n Enter text :");
+                ASCIIArtTool.Drow(Console.ReadLine());
+                Console.WriteLine("\n\n Created by : Moamen Mostafa\n");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
